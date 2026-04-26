d9e16c8622c88bd75abe5fc77596b2ef6b2f5d94
ritikshrivastav9517 <114847539+ritikshrivastav9517@users.noreply.github.com>
2026-04-27 02:34:13 +0530

    fresh fullstack project

--- Client/src/pages/navbarPages/Login.jsx
+++ Client/src/pages/navbarPages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     };
 
     const handleGoogleLogin = () => {
-            window.location.href = "http://13.48.135.13:3000/api/v1/auth/google";
+        window.location.href = "http://localhost:3000/api/v1/auth/google";
     };
 
     return (
