7d7fa2103b4015c6f9e5c557cd405980d839d63b
Ritik Shrivastav <114847539+ritikshrivastav9517@users.noreply.github.com>
2026-04-27 23:14:47 +0530

    Update Google login URL to production server

--- Client/src/pages/navbarPages/Login.jsx
+++ Client/src/pages/navbarPages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     };
 
     const handleGoogleLogin = () => {
-        window.location.href = "http://localhost:3000/api/v1/auth/google";
+        window.location.href = "http://13.53.160.129:3000/api/v1/auth/google";
     };
 
     return (
